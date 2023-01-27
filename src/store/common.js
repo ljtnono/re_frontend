8ee235b768571b31e26f6ccf95ec125fbce1e99c
lingjiatong <935188400@qq.com>
2023-01-27 20:20:13 +0800

    [feat]新增common的vuex模块

--- src/store/common.js
+++ src/store/common.js
@@ -3,7 +3,11 @@ export default {
   state: () => ({
     // 当前路由对象，每次路由改变时都会实时更改
     activeRoute: null,
-    //
+    //########## 侧边栏相关数据 ##########//
+    // 热门标签列表
+    hotTagList: [],
+    // 友情链接列表
+    friendLinkList: []
   }),
   getters: {},
   mutations: {
@@ -11,6 +15,14 @@ export default {
     changeActiveRoute(state, activeRoute) {
       state.activeRoute = activeRoute;
     },
+    // 修改热门标签列表
+    changeHotTagList(state, hotTagList) {
+      state.hotTagList = hotTagList;
+    },
+    // 修改友情链接列表
+    changeFriendLinkList(state, friendLinkList) {
+      state.friendLinkList = friendLinkList;
+    }
   },
   actions: {},
   modules: {},
